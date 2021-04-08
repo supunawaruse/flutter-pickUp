2e949746c2dab245a409ecd734ef37d21ed2f093
supuna98 <supunawa@gmail.com>
2021-04-08 16:52:12 +0530

    complete home screen layout

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,16 +1,115 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:skype_clone/resources/firebaseRepository.dart';
+import 'package:skype_clone/utils/universal_variables.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  FirebaseRepository _repository = FirebaseRepository();
+
+  PageController pageController;
+
+  int _page = 0;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    pageController = PageController();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Center(
-          child: Text('Home Screen'),
+      backgroundColor: UniversalVariables.blackColor,
+      body: PageView(
+        physics: NeverScrollableScrollPhysics(),
+        children: [
+          Center(
+              child: Text(
+            'first page',
+            style: TextStyle(color: Colors.white),
+          )),
+          Center(
+              child:
+                  Text('second page', style: TextStyle(color: Colors.white))),
+          Center(
+              child: Text('third page', style: TextStyle(color: Colors.white))),
+        ],
+        controller: pageController,
+        onPageChanged: onPageChange,
+      ),
+      bottomNavigationBar: Container(
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: CupertinoTabBar(
+            backgroundColor: UniversalVariables.blackColor,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.chat,
+                    color: (_page == 0)
+                        ? UniversalVariables.lightBlueColor
+                        : UniversalVariables.greyColor,
+                  ),
+                  title: Text("Chats",
+                      style: TextStyle(
+                          fontSize: 10,
+                          color: (_page == 0
+                              ? UniversalVariables.lightBlueColor
+                              : UniversalVariables.greyColor)))),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.call,
+                    color: (_page == 1)
+                        ? UniversalVariables.lightBlueColor
+                        : UniversalVariables.greyColor,
+                  ),
+                  title: Text("Calls",
+                      style: TextStyle(
+                          fontSize: 10,
+                          color: (_page == 1
+                              ? UniversalVariables.lightBlueColor
+                              : UniversalVariables.greyColor)))),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.contact_phone,
+                    color: (_page == 2)
+                        ? UniversalVariables.lightBlueColor
+                        : UniversalVariables.greyColor,
+                  ),
+                  title: Text("Contacts",
+                      style: TextStyle(
+                          fontSize: 10,
+                          color: (_page == 2
+                              ? UniversalVariables.lightBlueColor
+                              : UniversalVariables.greyColor)))),
+            ],
+            onTap: navigateTapped,
+            currentIndex: _page,
+          ),
         ),
       ),
     );
+  }
+
+  void signOut() {
+    _repository.signOut();
+  }
+
+  void onPageChange(int page) {
+    setState(() {
+      _page = page;
+    });
+  }
+
+  void navigateTapped(int page) {
+    pageController.jumpToPage(page);
   }
 }
--- lib/screens/loginScreen.dart
+++ lib/screens/loginScreen.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:skype_clone/resources/firebaseRepository.dart';
 import 'package:skype_clone/screens/homeScreen.dart';
+import 'package:skype_clone/utils/universal_variables.dart';
 
 class LoginScreen extends StatefulWidget {
   LoginScreen({Key key}) : super(key: key);
@@ -13,24 +15,46 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   FirebaseRepository _repository = FirebaseRepository();
 
+  bool isLoginPressed = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: loginButton()),
+      backgroundColor: UniversalVariables.blackColor,
+      body: Stack(
+        children: [
+          Center(
+            child: loginButton(),
+          ),
+          isLoginPressed
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Container()
+        ],
+      ),
     );
   }
 
   Widget loginButton() {
-    return TextButton(
-      onPressed: () => performLogin(),
-      child: Text(
-        'Login',
-        style: TextStyle(fontSize: 35),
+    return Shimmer.fromColors(
+      baseColor: Colors.white,
+      highlightColor: UniversalVariables.senderColor,
+      child: TextButton(
+        onPressed: () => performLogin(),
+        child: Text(
+          'Login',
+          style: TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
 
   void performLogin() {
+    setState(() {
+      isLoginPressed = true;
+    });
+
     _repository.signIn().then((UserCredential userCredential) => {
           if (userCredential.user != null)
             {authenticateUser(userCredential.user)}
@@ -40,23 +64,23 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   void authenticateUser(User user) {
-    _repository.authenticateUser(user).then((isNewUser) => {
-          if (isNewUser)
-            {
-              _repository.addDataToDatabase(user).then((value) {
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) {
-                  return HomeScreen();
-                }));
-              })
-            }
-          else
-            {
-              Navigator.pushReplacement(context,
-                  MaterialPageRoute(builder: (context) {
-                return HomeScreen();
-              }))
-            }
+    _repository.authenticateUser(user).then((isNewUser) {
+      setState(() {
+        isLoginPressed = false;
+      });
+      if (isNewUser) {
+        _repository.addDataToDatabase(user).then((value) {
+          Navigator.pushReplacement(context,
+              MaterialPageRoute(builder: (context) {
+            return HomeScreen();
+          }));
         });
+      } else {
+        Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) {
+          return HomeScreen();
+        }));
+      }
+    });
   }
 }
