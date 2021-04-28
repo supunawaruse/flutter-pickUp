77aedd837b5e721e800c85e48a0169bfd7f40de0
Supuna Warusawithana <supunawa@gmail.com>
2021-04-28 09:57:13 +0530

    add user details screen

--- lib/screens/pageViews/widgets/user_circle.dart
+++ lib/screens/pageViews/widgets/user_circle.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:skype_clone/provider/user_provider.dart';
+import 'package:skype_clone/screens/pageViews/widgets/user_details_container.dart';
 import 'package:skype_clone/utils/universal_variables.dart';
 import 'package:skype_clone/utils/utilities.dart';
 
@@ -10,36 +11,43 @@ class UserCircle extends StatelessWidget {
     final UserProvider userProvider =
         Provider.of<UserProvider>(context, listen: false);
 
-    return Container(
-      height: 40,
-      width: 40,
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(50),
-          color: UniversalVariables.separatorColor),
-      child: Stack(
-        children: [
-          Align(
-              alignment: Alignment.center,
-              child: Text(
-                Utils.getInitials(userProvider.getUser.name),
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: UniversalVariables.lightBlueColor,
-                    fontSize: 13),
-              )),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: Container(
-              height: 12,
-              width: 12,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                      color: UniversalVariables.blackColor, width: 2),
-                  color: UniversalVariables.onlineDotColor),
-            ),
-          )
-        ],
+    return GestureDetector(
+      onTap: () => showModalBottomSheet(
+          context: context,
+          backgroundColor: UniversalVariables.blackColor,
+          builder: (context) => UserDetailsContainer(),
+          isScrollControlled: true),
+      child: Container(
+        height: 40,
+        width: 40,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(50),
+            color: UniversalVariables.separatorColor),
+        child: Stack(
+          children: [
+            Align(
+                alignment: Alignment.center,
+                child: Text(
+                  Utils.getInitials(userProvider.getUser.name),
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: UniversalVariables.lightBlueColor,
+                      fontSize: 13),
+                )),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                height: 12,
+                width: 12,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                        color: UniversalVariables.blackColor, width: 2),
+                    color: UniversalVariables.onlineDotColor),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
