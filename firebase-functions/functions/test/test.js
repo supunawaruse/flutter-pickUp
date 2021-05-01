b5ea188c0a6188b0e6e9d61c50a64b21338704ae
Supuna Warusawithana <supunawa@gmail.com>
2021-05-01 16:31:21 +0530

    add token when call

--- firebase-functions/functions/test/test.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions')
-
-exports.testFunction = functions.https.onRequest((req, res) => {
-  functions.logger.info('Hello logs!', { structuredData: true })
-  res.send('welcome supuna')
-})
